f7d60bdc1ae18ee7274d1a734abb71415afd336c
Silas Silva <silas.silva@rockt.com.br>
2018-03-31 11:01:13 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello World with Nodejs!');
+});
+
+app.listen(3000, function() {
+  console.log('This app listening on port 3000!');
+});
